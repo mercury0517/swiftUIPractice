4e1b02092b66bae93c2404088bef706548f23f2d
Akihiro <aki19960517@gmail.com>
2022-07-10 18:18:26 +0900

    DailyViewをリストで並べた

--- SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -1,17 +1,10 @@
-//
-//  SwiftUIPracticeApp.swift
-//  SwiftUIPractice
-//
-//  Created by 伊原明宏 on 2022/06/11.
-//
-
 import SwiftUI
 
 @main
 struct SwiftUIPracticeApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
